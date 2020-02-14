f5f4ae7568960aaccac3b814eaf58fc451000f7c
Youngkun <youngkunzhu@163.com>
2020-02-14 12:58:45 +0800

    自定义分页器

--- apps/blogs/views.py
+++ apps/blogs/views.py
@@ -1,7 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
-from custom.pagination import BlogTagPagination, ArticlePagination
+from custom.paginations import BlogTagPagination, ArticlePagination
 from .models import BlogCategory, BlogTag, Article
 from .serializers import BlogCategorySerializer, BlogTagSerializer, ArticleSerializer
+from rest_framework.filters import OrderingFilter
 
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -21,5 +22,6 @@ class ArticleViewSet(ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     pagination_class = ArticlePagination
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category']
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['category', 'tags']
+    ordering_fields = ['add_time', 'click_num', 'favor_num', 'comment_num', 'user']
