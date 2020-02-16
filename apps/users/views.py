0bf93bfc6a2e2232e5861ace27cf7875979890aa
Youngkun <youngkunzhu@163.com>
2020-02-16 15:54:01 +0800

    完成把生成验证码的动作放在序列化器中

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,11 +1,5 @@
 from rest_framework.viewsets import ModelViewSet, GenericViewSet, mixins
-
-from custom.utils import send_verify_code_by_email, generate_code
-from mysite_server.settings import CODE_LENGTH
-from .models import UserProfile, VerifyCode
 from .serializers import UserRegisterSerializer, VerifyCodeSerializer
-from rest_framework.response import Response
-from rest_framework import status
 
 
 class VerifyCodeViewSet(mixins.CreateModelMixin, GenericViewSet):
